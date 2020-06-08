a33cd3168b546ca59b9d933e7217e2100af0d83c
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-06-08 23:05:53 +0200

    add mongoose passport local

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new mongoose.Schema({
 	email: {
@@ -14,5 +15,7 @@ const UserSchema = new mongoose.Schema({
 		minlength: 6
 	}
 });
+
+UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', UserSchema);
