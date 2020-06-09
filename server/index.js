e01656aaacb572ffda5e180561690b97ef89409f
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-06-09 22:43:40 +0200

    configure passport

--- server/index.js
+++ server/index.js
@@ -11,9 +11,17 @@ require('dotenv').config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
 	require('express-session')({
-		secret: process.env.SESSION_SECRET
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false
 	})
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost:27017/mern', { useNewUrlParser: true });
