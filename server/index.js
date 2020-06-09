4fd0e0308dedbf9dc1772b24d73ee908d074046b
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-06-09 23:28:33 +0200

    add register route

--- server/index.js
+++ server/index.js
@@ -1,35 +1,50 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('./models/user');
 const app = express();
 
 require('dotenv').config();
 
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-	require('express-session')({
-		secret: process.env.SESSION_SECRET,
-		resave: false,
-		saveUninitialized: false
-	})
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost:27017/mern', { useNewUrlParser: true });
+
 const db = mongoose.connection;
+
 db.on('open', () => console.log('connected'));
 
 app.get('/', (req, res) => {
 	res.send({ hi: 'there' });
+});
+
+app.post('/register', async (req, res) => {
+	try {
+		let { email, password } = req.body;
+
+		if (!email || !password) return res.status(400).json({ msg: 'Not all fields have been entered.' });
+		if (password.length < 5)
+			return res.status(400).json({ msg: 'The password needs to be at least 5 characters long.' });
+
+		const existingUser = await User.findOne({ email: email });
+		if (existingUser) return res.status(400).json({ msg: 'An account with this email already exists.' });
+
+		const salt = await bcrypt.genSalt();
+		const passwordHash = await bcrypt.hash(password, salt);
+
+		const newUser = new User({
+			email,
+			password: passwordHash
+		});
+		const savedUser = await newUser.save();
+		res.json(savedUser);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
 });
 
 const PORT = process.env.PORT || 5000;
--- server/models/user.js
+++ server/models/user.js
@@ -2,20 +2,10 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		required: true,
-		trim: true,
-		unique: true
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 6
-	}
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true, minlength: 5 }
 });
 
-UserSchema.plugin(passportLocalMongoose);
+// UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', UserSchema);
