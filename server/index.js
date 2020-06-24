cbd2ddd84badff9ac43ee7aab8dc079ffcf77c98
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-06-24 17:36:19 +0200

    add passport

--- server/index.js
+++ server/index.js
@@ -1,22 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('./models/user');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-require('dotenv').config();
-
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const db = mongoose.connection;
 const PORT = process.env.PORT || 5000;
-
-db.on('open', () => console.log('connected'));
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
-});
 
 app.listen(PORT);
