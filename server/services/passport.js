03bd9b001902da951213c1b3a1c909ead18a7ebe
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-06-28 13:47:30 +0200

    connect logging to database

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,18 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
 	new GoogleStrategy(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
+			callbackURL: 'http://localhost:5000/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('at', accessToken);
-			console.log('rt', refreshToken);
-			console.log('profile', profile);
+			new User({ googleId: profile.id }).save();
 		}
 	)
 );
