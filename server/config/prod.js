d77aa642b1a9ff7d320768febe0dc1cafaa17776
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-07-05 23:42:50 +0200

    add prod env

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientID: '645206526045-orb4ngp932lm7cjfihu414ndmgu77mrl.apps.googleusercontent.com',
+	googleClientSecret: 'ROSIL_K9inTrsjsQAeCMXNW9',
+	mongoURI: '+srv://dbUserProd:KZTW8y46u7s2K9Fb@cluster0.ngodx.mongodb.net/mern-app-prod?retryWrites=true&w=majority',
+	cookieKey: 'kjhfdjksaafdllsjsh'
+};
