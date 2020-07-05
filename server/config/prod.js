42df3ebfaa7caf5d8c875e5da1c551e4d1c3c82d
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-07-05 23:45:09 +0200

    add dotenv variables

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-	googleClientID: '645206526045-orb4ngp932lm7cjfihu414ndmgu77mrl.apps.googleusercontent.com',
-	googleClientSecret: 'ROSIL_K9inTrsjsQAeCMXNW9',
-	mongoURI: '+srv://dbUserProd:KZTW8y46u7s2K9Fb@cluster0.ngodx.mongodb.net/mern-app-prod?retryWrites=true&w=majority',
-	cookieKey: 'kjhfdjksaafdllsjsh'
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY
 };
