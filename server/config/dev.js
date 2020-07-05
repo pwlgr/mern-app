6816b829d8eae7e4dad324a3b84d08c2c7f1cb46
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-07-05 23:36:50 +0200

    add dev env

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientID: '804325459011-b3370q6pc0qlopac43achuk93ontlqf3.apps.googleusercontent.com',
+	googleClientSecret: 'r5CVHjEoV3Vxidm8hSBCtDhL',
+	mongoURI: 'mongodb+srv://dbUser:6pYIdMfXavOEdWWD@cluster0-cdmgn.mongodb.net/mern?retryWrites=true&w=majority',
+	cookieKey: 'ashfkadfksfhask'
+};
