23aaf07919579aa2c22ee77204c40e7d05ac8747
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-07-08 23:25:04 +0200

    add proxy to client

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	return <div className="App">test</div>;
+	return (
+		<div className="App">
+			<a href="/auth/google">link</a>
+		</div>
+	);
 }
 
 export default App;
