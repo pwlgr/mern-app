431c3e73972f6317e2d207bd29d28f8cfc7de411
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-07-10 17:07:37 +0200

    fix proxy setup

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
 module.exports = function(app) {
-	app.use(createProxyMiddleware('/auth/google', { target: 'http://localhost:5000/' }));
+	app.use(
+		[ '/api', '/auth/google' ],
+		createProxyMiddleware({
+			target: 'http://localhost:5000'
+		})
+	);
 };
