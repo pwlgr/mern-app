52a79d64edbcb17aca37b4989a53d9a11dc8499e
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-07-16 17:24:52 +0200

    add paths to routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 const Header = () => <h2>Header</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
-const Feedback = () => <h2>Feedback</h2>;
+const FeedbackNew = () => <h2>FeedbackNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
@@ -11,7 +11,10 @@ const App = () => {
 		<div>
 			<BrowserRouter>
 				<div>
-					<Route path="/" component={Landing} />
+					<Header />
+					<Route exact path="/" component={Landing} />
+					<Route exact path="/feedbacks" component={Dashboard} />
+					<Route path="/feedbacks/new" component={FeedbackNew} />
 				</div>
 			</BrowserRouter>
 		</div>
