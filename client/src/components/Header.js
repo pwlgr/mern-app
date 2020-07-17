fb5359dea6d47883168865f43f331de7551c0434
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-07-18 01:16:28 +0200

    add header styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<nav>
+			<div class="nav-wrapper">
+				<a href="#" class="left brand-logo">
+					Feedback
+				</a>
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li>
+						<a href="#">Login with Google</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Header;
