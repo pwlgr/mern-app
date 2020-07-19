75646d8d05cf4da80763591c92bbea1b4895d2d9
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-07-19 22:58:34 +0200

    add redirect if user logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { useEffect } from 'react';
 
-const Header = () => {
+const Header = (props) => {
+	const renderContent = () => {
+		switch (props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">Login with Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a href="/api/logout">Logout</a>
+					</li>
+				);
+		}
+	};
+
 	return (
 		<nav>
-			<div class="nav-wrapper">
-				<a href="#" class="left brand-logo">
+			<div className="nav-wrapper">
+				<a href="#" className="left brand-logo">
 					Feedback
 				</a>
-				<ul id="nav-mobile" class="right hide-on-med-and-down">
-					<li>
-						<a href="#">Login with Google</a>
-					</li>
+				<ul id="nav-mobile" className="right hide-on-med-and-down">
+					{renderContent()}
 				</ul>
 			</div>
 		</nav>
 	);
 };
 
-export default Header;
+const mapStateToProps = ({ auth }) => ({
+	auth
+});
+
+export default connect(mapStateToProps)(Header);
