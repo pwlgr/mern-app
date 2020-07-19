69d2b3accb33f3878b20f901b467f1052ce63230
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-07-19 22:10:18 +0200

    add fetch user action cretor

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
+
 const fetchUser = () => {
-	axios.get('/api/current_user');
+	return (dispatch) => {
+		axios.get('/api/current_user').then((res) => dispatch({ type: FETCH_USER, payload: res }));
+	};
 };
