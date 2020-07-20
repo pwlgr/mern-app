4ad2bceaa0e15ad3f18170a65e2a298dbabf8327
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-07-20 23:25:37 +0200

    add stripe config

--- server/config/dev.js
+++ server/config/dev.js
@@ -2,5 +2,9 @@ module.exports = {
 	googleClientID: '804325459011-b3370q6pc0qlopac43achuk93ontlqf3.apps.googleusercontent.com',
 	googleClientSecret: 'r5CVHjEoV3Vxidm8hSBCtDhL',
 	mongoURI: 'mongodb+srv://dbUser:6pYIdMfXavOEdWWD@cluster0-cdmgn.mongodb.net/mern?retryWrites=true&w=majority',
-	cookieKey: 'ashfkadfksfhask'
+	cookieKey: 'ashfkadfksfhask',
+	stripePublishableKey:
+		'pk_test_51H76FJBEjGCJYqao94nP3RT1vI6WijjE4yotOlFwKVRJWPxQ9q4EUI2vKGk21JoT5CfwcAMU2BmClsztxDE4kqMk00Iwh4KP0f',
+	stripeSecretKey:
+		'sk_test_51H76FJBEjGCJYqaoFCMgnHrdwpjvY2CMUx6Xkq0y33OmTb64AGFCARQgKJ9p2lGd00wTvc8Unrm0b17mIubuPMtX00qrjGJkY4'
 };
